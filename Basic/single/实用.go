e34b0c48f940bb9e7e7912faee7982b81f65fa1e
深蓝PC <mail@ri-co.cn>
2020-03-30 10:02:14 +0800

    组合函数处理

--- Basic/single/实用.go
+++ Basic/single/实用.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strings"
 )
 
 //排序
@@ -71,9 +72,34 @@ func Any(vs []string, f func(string) bool) bool {
 	return false
 }
 
-//返回一个包含所有切片中满足条件f的字符串新切片
+//Filter 返回一个包含所有切片中满足条件f的字符串新切片
 func Filter(vs string, f func(string) bool) []string {
+	for _, v := range vs {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
 
+//Map 返回一个对原始切片中所有字符串执行函数f后的新切片
+func Map(vs []string, f func(string) bool) []string {
+	vsm := make([]string, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+func collectionTest() {
+	strs := []string{"peach", "apple", "pear", "plum"} //最后这个梅子我竟然不认识
+	fmt.Println(Index(strs, "pear"))
+	fmt.Println(Include(strs, "grape"))
+	fmt.Println(Any(strs, func(v string) bool {
+		return strings.HasPrefix(v, "p")
+	}))
+	fmt.Println(Filter(strs, func(v string) bool {
+		return strings.Contains(v, "e")
+	}))
 }
 
 func main() {
