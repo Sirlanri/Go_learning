4fd9fb498ef8e1893f0e69b4f73b68226d4000ba
深蓝PC <mail@ri-co.cn>
2020-04-02 09:36:20 +0800

    读写文件

--- Basic/single/读写文件.go
+++ Basic/single/读写文件.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+//检查函数，可以少敲很多下键盘哦
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readTest() {
+	//相对路径，开头不要加/
+	data1, err := ioutil.ReadFile("data/text1.txt")
+	check(err)
+	fmt.Println(string(data1))
+
+	//稍微高级点的做法
+	file1, err := os.Open("data/text1.txt")
+	check(err)
+	b1 := make([]byte, 5) //最多读取5个字符
+	n1
+}
+
+func main() {
+	readTest()
+}
