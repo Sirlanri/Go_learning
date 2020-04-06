d19683f55b159c0b5b1793315c17f0b2154e576b
深蓝PC <mail@ri-co.cn>
2020-04-06 22:05:56 +0800

    哎，要入坑微信了，短时间内不能学go

--- Basic/single/读写文件.go
+++ Basic/single/读写文件.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 )
@@ -23,7 +25,55 @@ func readTest() {
 	file1, err := os.Open("data/text1.txt")
 	check(err)
 	b1 := make([]byte, 5) //最多读取5个字符
-	n1
+	n1, err := file1.Read(b1)
+	check(err)
+	fmt.Println("读取5个字符后", n1)
+
+	//到一个已知的位置，开始读取
+	o2, err := file1.Seek(6, 0)
+	check(err)
+	b2 := make([]byte, 4)
+	n2, err := file1.Read(b2)
+	check(err)
+	fmt.Println("seek结果：", n2, o2)
+
+	//更健壮的实现
+	o3, err := file1.Seek(6, 0)
+	check(err)
+	b3 := make([]byte, 2)
+	n3, err := io.ReadAtLeast(file1, b3, 2)
+	check(err)
+	fmt.Println("更强的方法", n3, o3)
+
+	//带缓冲的
+	r4 := bufio.NewReader(file1)
+	b4, err := r4.Peek(5)
+	check(err)
+	fmt.Println("带缓冲的", b4)
+}
+
+func writeTest() {
+	test2 := []byte{"WDNMD第一行"}
+	err := ioutil.WriteFile("data/text2.text", test2)
+	if err != nil {
+		println(err)
+	}
+
+	//更细颗粒的写入
+	file2, err := os.Create("data/text3.txt")
+	if err != nil {
+		println(err)
+	}
+	defer file2.Close()
+	intarr := [5]int{1, 2, 3, 4, 5}
+	lenint, err := file2.Write(intarr)
+	if err != nil {
+		println(err)
+	}
+	fmt.Println("写入的数据长度", lenint)
+	//将缓冲区的信息写入硬盘
+	file2.Sync()
+
 }
 
 func main() {
